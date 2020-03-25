b00e0be9a6c02f4e703d1bb4786361f34d662982
AnonymousTheMad <54331845+AnonymousTheMad@users.noreply.github.com>
2020-03-25 23:17:34 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+const name = 'Jimoh Tajuddeen Tajuddeen';
+const courses = ['html','css','js'];
+console.log(name);
+console.log(courses);
+for (let i = 1; i <= 200; i++){if (i % 2 !== 0) console.log(i);
+
+}
